a6560a0faea04765e226baeaf790c323aae57b7c
Oğuzhan Karaçar <oguzkaracar@gmail.com>
2020-11-08 20:26:41 +0300

    photo component fixed

--- components/photo/index.js
+++ components/photo/index.js
@@ -1,22 +1,47 @@
-import React from "react";
-import cn from "classnames";
+import React from 'react';
+import cn from 'classnames';
 
-import styles from "./photo.module.css";
+import styles from './photo.module.css';
 
-import useSWR from 'swr'
-import fetcher from '../../lib/fetch'
+import useSWR from 'swr';
+import fetcher from '../../lib/fetch';
 
 const Photo = ({
-	src,
-	alt,
-	size = 47,
+  alt,
+  size = 47,
+  header = false,
+  profile = false,
+  secondSrc,
 }) => {
-	const { data } = useSWR('/api/tweet', fetcher)
-	return (
-		<div className={cn([styles.photo])} style={{ width: size, height: size }}>
-			<img className={styles.img} src={data?.statuses[0].user.profile_image_url_https} alt={alt} />
-		</div>
-	);
+  const { data } = useSWR('/api/tweet', fetcher);
+
+  return (
+    <div
+      className={cn([styles.photo], header && styles.header)}
+      style={{ width: size, height: size }}>
+      {header ? (
+        <img
+          className={cn(styles.img, header && styles.header)}
+          src="https://pbs.twimg.com/profile_banners/860587802622398464/1604713321/600x200"
+          alt={alt}
+        />
+      ) : (
+        <img
+          className={cn(styles.img, header && styles.header)}
+          src={data?.statuses[0].user.profile_image_url_https}
+          alt={alt}
+        />
+      )}
+      {profile && (
+        <img
+          className={cn(styles.img, styles.profile)}
+          src={data?.statuses[0].user.profile_image_url_https}
+          alt={alt}
+          style={{ width: size, height: size }}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Photo;
