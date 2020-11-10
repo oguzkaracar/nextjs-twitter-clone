161036d5bc871601739b67684c2a00201a1b9713
Oğuzhan Karaçar <oguzkaracar@gmail.com>
2020-11-10 16:52:04 +0300

    Profile page is changed to providing data dynamically

--- components/pages/Profile.js
+++ components/pages/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import useSWR from 'swr';
 
 import Header from '../header';
 import TextTitle from '../text/TextTitle';
@@ -6,26 +7,27 @@ import IconButton from '../button/icon';
 import { Back } from '../icons';
 import ProfileContainer from '../profile-container';
 
+import fetcher from '../../lib/fetch';
+
 const ProfileMain = () => {
+  const { data } = useSWR('api/tweet', fetcher);
+
   return (
     <div>
-      <Header back>
-        <IconButton>
-          <Back />
-        </IconButton>
-        <div>
-          <TextTitle>Profile</TextTitle>
-          <span>14 Tweets</span>
-        </div>
-      </Header>
-	  {/* location ve websitesi bağlantısı propsları da eklenecek!!! */}
-      <ProfileContainer
-        name="Oğuzhan"
-        slug="oguzhankaracar"
-        created="May 2017"
-        following="50"
-        followers="10"
-      />
+      {data && (
+        <>
+          <Header back>
+            <IconButton>
+              <Back />
+            </IconButton>
+            <div>
+              <TextTitle>{data[0].user.name}</TextTitle>
+              <span>{data[0].user.statuses_count} Tweets</span>
+            </div>
+          </Header>
+          <ProfileContainer />
+        </>
+      )}
     </div>
   );
 };
--- components/profile-container/index.js
+++ components/profile-container/index.js
@@ -1,75 +1,91 @@
-import React from 'react'
-import useSWR from 'swr'
+import React from 'react';
+import useSWR from 'swr';
+import format from 'date-fns/format';
 
-import style from './style.module.css'
+import style from './style.module.css';
 
-import Photo from '../photo'
-import Button from '../button'
-import TitleBold from '../text/TextTitle'
-import fetcher from '../../lib/fetch'
-import Tweet from '../tweet'
-import TextBody from '../text/TextBody'
-import Stack from '../stack'
-import SmallText from '../text/TextSmall'
-import * as Icon from '../icons'
-import ExtraButton from '../button/extra'
+import Photo from '../photo';
+import Button from '../button';
+import TitleBold from '../text/TextTitle';
+import fetcher from '../../lib/fetch';
+import Tweet from '../tweet';
+import TextBody from '../text/TextBody';
+import Stack from '../stack';
+import SmallText from '../text/TextSmall';
+import * as Icon from '../icons';
+import ExtraButton from '../button/extra';
 
-function ProfileContainer({ name, slug, created, following, followers }) {
+function ProfileContainer() {
   const { data } = useSWR('/api/tweet', fetcher);
+
+  const userData = data?.[0].user;
 
   return (
     <section>
-      <div className={style.photo}>
-        <Photo
-          profile
-          header
-          secondSrc={
-            'https://pbs.twimg.com/profile_banners/860587802622398464/1604713321/600x200'
-          }
-        />
-      </div>
-      <div className={style.button}>
-        <Button>Edit profile</Button>
-      </div>
-      <div className={style.infoContainer}>
-        <Stack column gap={0}>
-          <TitleBold bold>{name}</TitleBold>
-          <SmallText>@{slug}</SmallText>
-          <Stack row small gap={8}>
-            <Icon.Calendar />
-            <SmallText>Joined {created} </SmallText>
-          </Stack>
-          <Stack row small>
-            <Stack row small gap={3}>
-              <TextBody bold>{following}</TextBody>
-              <SmallText>Following</SmallText>
+      {data && (
+        <>
+          <div className={style.photo}>
+            <Photo profile header secondSrc={userData.profile_banner_url} size={140} />
+          </div>
+          <div className={style.button}>
+            <Button>Edit profile</Button>
+          </div>
+          <div className={style.infoContainer}>
+            <Stack column gap={0}>
+              <TitleBold bold>{userData.name}</TitleBold>
+              <SmallText>@{userData.screen_name}</SmallText>
+              <Stack row gap={8} className={style.profileInfo}>
+                {/* <div>Ankara</div>
+                <div>
+                  <a href="https://oguzhankaracar.com">oguzhankaracar.com</a>
+                </div> */}
+
+                <Icon.Calendar />
+                <div>
+                  <SmallText>
+                    Joined {format(new Date(userData.created_at), 'MMMM yyyy')}
+                  </SmallText>
+                </div>
+              </Stack>
+              <Stack row small>
+                <Stack row small gap={3}>
+                  <TextBody bold>{userData.friends_count}</TextBody>
+                  <SmallText>Following</SmallText>
+                </Stack>
+                <Stack row gap={3} small>
+                  <TextBody bold>{userData.followers_count}</TextBody>
+                  <SmallText>Followers</SmallText>
+                </Stack>
+              </Stack>
             </Stack>
-            <Stack row gap={3} small>
-              <TextBody bold>{followers}</TextBody>
-              <SmallText>Followers</SmallText>
-            </Stack>
-          </Stack>
-        </Stack>
-        <div className={style.buttonContainer}>
-          <ExtraButton>
-            <span>Tweets</span>
-          </ExtraButton>
-          <ExtraButton>
-            <span>Tweets & Replies</span>
-          </ExtraButton>
-          <ExtraButton>
-            <span>Media</span>
-          </ExtraButton>
-          <ExtraButton>
-            <span>Likes</span>
-          </ExtraButton>
-        </div>
-      </div>
-      {data?.statuses.map((tweet) => {
-        return <Tweet key={tweet.id} {...tweet} />
-      })}
+
+            {/* Tabs */}
+
+            <div className={style.buttonContainer}>
+              <ExtraButton>
+                <span>Tweets</span>
+              </ExtraButton>
+              <ExtraButton>
+                <span>Tweets & Replies</span>
+              </ExtraButton>
+              <ExtraButton>
+                <span>Media</span>
+              </ExtraButton>
+              <ExtraButton>
+                <span>Likes</span>
+              </ExtraButton>
+            </div>
+          </div>
+
+          {/* Tweets from my account. */}
+
+          {data?.map(tweet => {
+            return <Tweet key={tweet.id} {...tweet} />;
+          })}
+        </>
+      )}
     </section>
-  )
+  );
 }
 
-export default ProfileContainer
+export default ProfileContainer;
