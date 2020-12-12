09421e5ebea2cce01e8ac3a6cefeb28a9e527021
Oğuzhan Karaçar <oguzkaracar@gmail.com>
2020-12-13 00:09:17 +0300

    Tweet gönderme devre dışı bırakıldı.

--- components/loading/index.js
+++ components/loading/index.js
@@ -1,34 +1,34 @@
-import React from "react";
+import React from 'react';
 
-import styles from "./style.module.css";
+import styles from './style.module.css';
 
 const Loading = ({ size = 32, stroke = 2 }) => {
-	return (
-		<div className={styles.loading}>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width={size}
-				height={size}
-				viewBox="0 0 38 38"
-				stroke="rgb(29, 161, 242, 1)">
-				<g fill="none" fill-rule="evenodd">
-					<g transform="translate(1 1)" strokeWidth={stroke}>
-						<circle strokeOpacity=".5" cx="18" cy="18" r="18" />
-						<path d="M36 18c0-9.94-8.06-18-18-18">
-							<animateTransform
-								attributeName="transform"
-								type="rotate"
-								from="0 18 18"
-								to="360 18 18"
-								dur="1s"
-								repeatCount="indefinite"
-							/>
-						</path>
-					</g>
-				</g>
-			</svg>
-		</div>
-	);
+  return (
+    <div className={styles.loading}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+        viewBox="0 0 38 38"
+        stroke="rgb(29, 161, 242, 1)">
+        <g fill="none" fill-rule="evenodd">
+          <g transform="translate(1 1)" strokeWidth={stroke}>
+            <circle strokeOpacity=".5" cx="18" cy="18" r="18" />
+            <path d="M36 18c0-9.94-8.06-18-18-18">
+              <animateTransform
+                attributeName="transform"
+                type="rotate"
+                from="0 18 18"
+                to="360 18 18"
+                dur="1s"
+                repeatCount="indefinite"
+              />
+            </path>
+          </g>
+        </g>
+      </svg>
+    </div>
+  );
 };
 
 export default Loading;
--- pages/api/newTweet.js
+++ pages/api/newTweet.js
@@ -1,17 +1,16 @@
-import Twit from "../../lib/twit";
-
+import Twit from '../../lib/twit';
 
 // tweet gönderme
 export default (req, res) => {
+  const { tweet } = req.body;
 
-	const { tweet } = req.body;
+  if (!tweet) return res.status(400).json({ message: 'Tweet boş olamaz!!' });
 
-	if(!tweet) return res.status(400).json({message:'Tweet boş olamaz!!'});
-
-	Twit.post("statuses/update", { status: "TEST " + tweet}, function (err, data, response) {
-		if (err) {
-			return res.status(400).json({message:'Bir hata oluştu!!!'});
-		}
-		res.status(200).json(JSON.stringify(data));
-	});
+  // statuses/update
+  Twit.post('#!', { status: 'TEST ' + tweet }, function (err, data, response) {
+    if (err) {
+      return res.status(400).json({ message: 'Bir hata oluştu!!!' });
+    }
+    res.status(200).json(JSON.stringify(data));
+  });
 };
